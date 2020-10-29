a53377d71c38e3f9405dcfd8e03015a88f369784
Wesley Vercoutere <wesley.vercoutere@gmail.com>
2020-10-29 14:55:01 +0100

    Les6 Oef-1

---
+++ RPi1-6/oef1.py
@@ -0,0 +1,12 @@
+#############################################################
+##  Wesley Vercoutere                                      ## 
+##  CVO Focus                                              ## 
+##  Deel 1: Basis programmeren in Python met RaspberryPi   ##
+##  Les 6 - Oef 1                                          ##  
+#############################################################
+
+from tkinter import *
+
+
+if __name__ == "__main__":
+    pass
