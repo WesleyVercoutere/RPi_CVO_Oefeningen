78312c08c002b8f4d79653604ee2742bd70ea68c
WesleyVercoutere <wesley.vercoutere@gmail.com>
2020-10-30 10:24:48 +0100

    Add breadbord setup

--- RPi1-6/oef1.py
+++ RPi1-6/oef1.py
@@ -5,7 +5,25 @@
 ##  Les 6 - Oef 1                                          ##  
 #############################################################
 
+'''
+Setup breadbord:
+
+ledBlue = pin 19
+ledRed = pin 20
+ledYellow = pin 21
+ledGreen = pin 22
+
+button 1 = pin 23
+button 2 = pin 24
+button 3 = pin 25
+button 4 = pin 26
+'''
+
 from tkinter import *
+
+
+
+
 
 
 if __name__ == "__main__":
