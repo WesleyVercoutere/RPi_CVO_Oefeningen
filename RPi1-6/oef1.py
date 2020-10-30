bb156e2cc969c44768faa1cb6e4a3c2ae67a0d09
Wesley Vercoutere <wesley.vercoutere@gmail.com>
2020-10-30 12:14:56 +0100

    Update oef1

--- RPi1-6/oef1.py
+++ RPi1-6/oef1.py
@@ -20,11 +20,37 @@ button 4 = pin 26
 '''
 
 from tkinter import *
+import RPi.GPIO as GPIO
+
+ledBlue = 19
+ledRed = 20
+ledYellow = 21
+ledGreen = 22
+
+button1 = 23
+button2 = 24
+button3 = 25
+button4 = 26
+
+leds = (ledBlue, ledRed, ledYellow, ledGreen)
+buttons = (button1, button2, button3, button4)
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+for led in leds:
+    GPIO.setup(led, GPIO.OUT)
+
+for btn in buttons:
+    GPIO.setup(btn, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+
+while True:
+    for index in range(len(buttons)):
+        GPIO.output(leds[index], GPIO.input(buttons[index]))
 
 
 
 
-
-
-if __name__ == "__main__":
-    pass
+#if __name__ == "__main__":
+#     pass
