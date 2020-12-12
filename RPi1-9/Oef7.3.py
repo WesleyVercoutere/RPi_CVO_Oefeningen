a126d033b31de96d4a15f39f29672cb86946d8f2
WesleyVercoutere <wesley.vercoutere@gmail.com>
2020-12-12 13:49:41 +0100

    Update RPi-9

--- RPi1-9/Oef7.3.py
+++ RPi1-9/Oef7.3.py
@@ -2,13 +2,11 @@
 ##  Wesley Vercoutere                                      ##
 ##  CVO Focus                                              ##
 ##  Deel 1: Basis programmeren in Python met RaspberryPi   ##
-##  Les 9 - Oef 7.1                                        ##
+##  Les 9 - Oef 7.3                                        ##
 #############################################################
 
 '''
-7-1) Schrijf een programma dat weergeeft (print en/of GUI) hoeveel milliseconden je een drukknop, 
-    verbonden met je RP, had ingedrukt. 
-    Je kan deze waarde updaten bij het loslaten.
+7-3) Schrijf een programma dat een teller verhoogt en ze’n waarde uitprint telkens een schakelaar wordt ingedrukt. Kan je die waarde ook tonen in een GUI? Je kan geen GUI aanpassen vanuit de callback van je schakelaar, hoe los je dit op?
 '''
 
 from tkinter import Tk, Label
@@ -22,14 +20,12 @@ except:
 
 
 root = Tk()
-root.title('Les 9 - Oef 7.1')
+root.title('Les 9 - Oef 7.3')
 root.geometry('800x400')
-
 
 # GPIO general
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-
 
 # Inputs
 pinPushBtn = 25
@@ -56,25 +52,11 @@ leds = (pinLedBlue, pinLedRed, pinLedYellow, pinLedGreen)
 GPIO.setup(outputs, GPIO.OUT, initial=GPIO.LOW)
 
 
-# Program variables
-time = False
-elapsedTime = 0
-prevElapsedTime = 0
-
-
 # Callbacks
-def toggle_timer():
-    global time
-    time = not time
-
-GPIO.add_event_detect(pinPushBtn, GPIO.BOTH, callback=toggle_timer, bouncetime=50)
 
 
 # GUI
-lblText = Label(root, text="Elapsed time :", padx=10, pady=10)
-lblText.grid(row=0, column=0)
-lblTime = Label(root, text=elapsedTime, padx=10, pady=10)
-lblTime.grid(row=0, column=1)
+
 
 
 while True:
