bfbb0a624d61779d550b59cd39a6470144261507
WesleyVercoutere <wesley.vercoutere@gmail.com>
2021-02-15 20:56:52 +0100

    Update opdracht

--- RPi-2/RPi2_Opdracht1_Transportband/ConveyorApp/src/ConveyorApp.py
+++ RPi-2/RPi2_Opdracht1_Transportband/ConveyorApp/src/ConveyorApp.py
@@ -1,11 +1,38 @@
-###########################################################
-#  Wesley Vercoutere                                      #
-#  CVO Focus                                              #
-#  Deel 1: Basis programmeren in Python met RaspberryPi   #
-#  Eindopdracht: Thermostaat applicatie                   #
-###########################################################
+#########################################################
+#  Wesley Vercoutere                                    #
+#  CVO Focus                                            #
+#  Deel 2: INTEGRATIE EN COMMUNICATIE MET RASPBERRY PI  #
+#  Opdracht 1: Transportband applicatie                 #
+#########################################################
 
-# TODO  
+'''
+-	Drukknop 1 : GPIO 18
+-	Drukknop 2 : GPIO 23
+-	Drukknop 3 : GPIO 25
+-	Drukknop 4 : GPIO 12
+-	Drukknop 5 : GPIO 16
+-	Drukknop 6 : GPIO 26
+-	Rotary A : GPIO 20
+-	Rotary B : GPIO 21
+-	Potentiometer : MCP 3008 channel 1
+-	MCP3008 :
+    o	CLK : GPIO 11 / SPIO SCLK
+    o	Dout : GPIO 9 / SPIO MISO
+    o	Din : GPIO 10 / SPIO MOSI
+    o	CS/Sh : GPIO 8 / SPIO CS1
+-	Led green : GPIO 5
+-	Led yellow : GPIO 6
+-	Led blue : GPIO 13
+-	Led red : GPIO 19
+-	Bluetooth module
+    o	RX : UARTO TX
+    o	TX : UARTO RX
+-	Stepper motor
+    o	In 1 : GPIO 17
+    o	In 2 : GPIO 27
+    o	In 3 : GPIO 22
+    o	In 4 : GPIO 23
+'''
 
 import RPi.GPIO as GPIO
 import time
