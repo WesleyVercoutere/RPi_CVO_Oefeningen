0c1d6045717449f8478bfdaf8def1563b3e73c16
Wesley Vercoutere <wesley.vercoutere@gmail.com>
2021-02-19 15:08:01 +0100

    Update opdracht

--- RPi-2/RPi2_Opdracht1_Transportband/ConveyorApp/src/service/ConveyorManager.py
+++ RPi-2/RPi2_Opdracht1_Transportband/ConveyorApp/src/service/ConveyorManager.py
@@ -1,6 +1,14 @@
-
+import RPi.GPIO as GPIO
 
 class ConveyorManager:
 
     def __init__(self, hardwareManager):
         self.hardwareMgr = hardwareManager
+
+        self.setCallbacks()
+
+    def setCallbacks(self):
+        self.hardwareMgr.btn1.setEvent(GPIO.RISING, self.toggleLed, 200)
+
+    def toggleLed(self):
+        self.hardwareMgr.ledGreen.toggle()
