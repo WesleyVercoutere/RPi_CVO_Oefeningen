3ec0efd13e4345b0735d2492ab798dc60762aee1
Wesley Vercoutere <wesley.vercoutere@gmail.com>
2021-02-20 11:41:10 +0100

    Update opdracht

--- RPi-2/RPi2_Opdracht1_Transportband/ConveyorApp/src/service/ConveyorManager.py
+++ RPi-2/RPi2_Opdracht1_Transportband/ConveyorApp/src/service/ConveyorManager.py
@@ -1,11 +1,30 @@
 import RPi.GPIO as GPIO
 
-class ConveyorManager:
+from service.observer.Observable import Observable
+
+
+class ConveyorManager(Observable):
 
     def __init__(self, hardwareManager):
         self.hardwareMgr = hardwareManager
 
         self.setCallbacks()
+
+#region Main methods conveyor
+
+    def moveToHomePosition(self):
+        print("Moving to home position")
+
+    def moveToPosition(self, position):
+        print(f"Moving to {position}")
+
+    def move(self, direction):
+        print(f"Moving {direction}")
+
+    def setPosition(self, positionId, position):
+        print(f"Set {positionId} to {position}")
+
+#endregion
 
     def setCallbacks(self):
         self.hardwareMgr.btn1.setEvent(GPIO.RISING, lambda _ : self.toggleLed(), 200)
