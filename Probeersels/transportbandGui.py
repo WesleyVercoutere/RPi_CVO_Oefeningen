2e86328b418494221b4501155aec4319b7d7092d
WesleyVercoutere <wesley.vercoutere@gmail.com>
2021-03-08 12:37:06 +0100

    Update opdracht

--- Probeersels/transportbandGui.py
+++ Probeersels/transportbandGui.py
@@ -1,32 +1,36 @@
-import time
 from tkinter import *
 
-
+"""
+https://www.dreamincode.net/forums/topic/371440-tkinter-overview-with-a-fixed-width-grid/
+"""
 class DesktopGUI:
 
     def __init__(self):
+        # self.mgr = conveyorManager
+        # self.mgr.addObserver(self)
+        # self.controller = desktopController
+
         self.root = None
         self.setupRoot()
 
         self.currentState = StringVar()
-        self.currentState.set("Test")
+        self.currentState.set("IDLE")
         self.currentPosition = StringVar()
-        self.currentPosition.set("Test")
+        self.currentPosition.set("Pos 1")
         self.currentSteps = IntVar()
-        self.currentSteps.set(999)
+        self.currentSteps.set(50)
         self.currentMessage = StringVar()
-        self.currentMessage.set("Very long ............................................. text")
+        self.currentMessage.set("Conveyor is stopped")
 
         self.setupStateSection()
         self.setupControlSection()
         self.setupSettingsSection()
 
-        self.loop()
-
     def setupRoot(self):
         self.root = Tk()
         self.root.title('Transportband')
-
+        self.root.geometry("800x400")
+        self.root.resizable(width=False, height=True)
         self.root.columnconfigure(0, weight=1)
 
     def setupStateSection(self):
@@ -41,28 +45,28 @@ class DesktopGUI:
         lbl_state.grid(column=0, row=0)
         lbl_currentState = Label(frame1, textvariable=self.currentState, fg="blue")
         lbl_currentState.grid(column=1, row=0)
-        frame1.grid(column=0, row=0, sticky="w", padx=10, pady=5)
+        frame1.grid(column=0, row=0, sticky="ew", padx=10, pady=5)
 
         frame2 = Frame(frame)
         lbl_position = Label(frame2, text="Position : ")
         lbl_position.grid(column=0, row=0)
         lbl_currentState = Label(frame2, textvariable=self.currentPosition, fg="blue")
         lbl_currentState.grid(column=1, row=0)
-        frame2.grid(column=1, row=0, sticky="w", padx=10, pady=5)
+        frame2.grid(column=1, row=0, sticky="ew", padx=10, pady=5)
 
         frame3 = Frame(frame)
         lbl_steps = Label(frame3, text="Nbr of steps from home position : ")
         lbl_steps.grid(column=0, row=0)
         lbl_currentSteps = Label(frame3, textvariable=self.currentSteps, fg="blue")
         lbl_currentSteps.grid(column=1, row=0)
-        frame3.grid(column=2, row=0, sticky="w", padx=10, pady=5)
+        frame3.grid(column=2, row=0, sticky="ew", padx=10, pady=5)
 
         frame4 = Frame(frame)
         lbl_state = Label(frame4, text="Message : ")
         lbl_state.grid(column=0, row=0)
         lbl_currentMessage = Label(frame4, textvariable=self.currentMessage, fg="blue")
         lbl_currentMessage.grid(column=1, row=0)
-        frame4.grid(column=0, row=1, sticky="w", padx=10, pady=5, columnspan=3)
+        frame4.grid(column=0, row=1, sticky="ew", padx=10, pady=5, columnspan=3)
 
         frame.grid(column=0, row=0, padx=10, pady=10, sticky='ew')
 
@@ -88,20 +92,36 @@ class DesktopGUI:
     def setupSettingsSection(self):
         frame = LabelFrame(self.root, text = "Conveyor Settings", padx=10, pady=10)
 
-        lbl_state = Label(frame, text="Position 1 : ")
+        frame1 = Frame(frame)
+        lbl_state = Label(frame1, text="Position 1 : ")
         lbl_state.grid(column=0, row=0, sticky="w", padx=10, pady=5)
-        lbl_position = Label(frame, text="Position 2 : ")
+        lbl1 = Label(frame1, text="ToDo", fg='red')
+        lbl1.grid(column=1, row=0)
+        frame1.grid(column=0, row=0, sticky="ew", padx=10, pady=5)
+
+        frame2 = Frame(frame)
+        lbl_position = Label(frame2, text="Position 2 : ")
         lbl_position.grid(column=0, row=1, sticky="w", padx=10, pady=5)
+        lbl2 = Label(frame2, text="ToDo", fg='red')
+        lbl2.grid(column=1, row=1)
+        frame2.grid(column=0, row=1, sticky="ew", padx=10, pady=5)
 
         frame.grid(column=0, row=2, padx=10, pady=10, sticky='ew')
 
-
     def update(self, *args, **kwargs):
         message = kwargs["message"]
+
+        dto = self.mgr.getState()
+
+        self.currentState.set(dto.state)
+        self.currentPosition.set(dto.position)
+        self.currentSteps.set(dto.steps)
+        self.currentMessage.set(message)
 
     def loop(self):
         self.root.mainloop()
 
 
 if __name__ == "__main__":
-    DesktopGUI()
+    gui = DesktopGUI()
+    gui.loop()
