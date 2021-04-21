6eabc27b8b2750ef9e3e35a6edb3fde12adb926b
WesleyVercoutere <wesley.vercoutere@gmail.com>
2021-04-21 10:09:46 +0200

    Create ipScan.bat

---
+++ Probeersels/ipScan.bat
@@ -0,0 +1,18 @@
+@echo off
+
+SET count=0
+SET /p subnet=Please enter IP address range (for example, 192.168.0)
+
+:start
+SET /a count=%count%+1
+
+cls
+ECHO. & ECHO Trying %subnet%.%count% & ECHO.
+
+ping -n 1 -w 1000 %subnet%.%count% >nul  
+IF %errorlevel%==0 echo %subnet%.%count% UP >> D:\Temp\pingnet.log  
+IF %errorlevel%==1 echo %subnet%.%count% DOWN >> D:\Temp\pingnet.log
+
+IF %count%==254 goto :eof
+
+GOTO start
