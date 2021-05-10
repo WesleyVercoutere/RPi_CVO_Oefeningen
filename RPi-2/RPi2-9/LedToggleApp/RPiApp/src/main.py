0196a464591a50c0f1e1ec372f141aa844b4f138
Wesley Vercoutere <wesley.vercoutere@gmail.com>
2021-05-10 22:20:49 +0200

    Update RPi2-9

--- RPi-2/RPi2-9/LedToggleApp/RPiApp/src/main.py
+++ RPi-2/RPi2-9/LedToggleApp/RPiApp/src/main.py
@@ -1,15 +1,22 @@
 import RPi.GPIO as GPIO
+import bluetooth
+import time
 
-from hardware.DigitalInputGPIO import DigitalInputGPIO
-from hardware.DigitalOutputGPIO import DigitalOutputGPIO
+from tkinter import *
 
 
 class Main:
 
     def __init__(self):
+        self._leds = [26, 19, 13, 6]
+        self._ledsStatus = [False, False, False, False]
+        self._btns = [21, 20, 16, 12]
+
         self._setup()
         self._initIO()
         self._initCallbacks()
+        self._initGui()
+        self._initBT()
         self._loop()
         
     def _setup(self):
@@ -17,26 +24,71 @@ class Main:
         GPIO.setmode(GPIO.BCM)
 
     def _initIO(self):
-        # GPIO IO
-        self._ledRedGPIO = DigitalOutputGPIO(26)
-        self._ledGreenGPIO = DigitalOutputGPIO(19)
-        self._ledBlueGPIO = DigitalOutputGPIO(13)
-        self._ledYellowGPIO = DigitalOutputGPIO(6)
-
-        self._btn1GPIO = DigitalInputGPIO(21, GPIO.PUD_UP)
-        self._btn2GPIO = DigitalInputGPIO(20, GPIO.PUD_UP)
-        self._btn3GPIO = DigitalInputGPIO(16, GPIO.PUD_UP)
-        self._btn4GPIO = DigitalInputGPIO(12, GPIO.PUD_UP)
+        GPIO.setup(self._leds, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(self._btns, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
     def _initCallbacks(self):
-        self._btn1GPIO.setEvent(edge=GPIO.FALLING, callback=self._ledRedGPIO.toggle, bouncetime=200)
-        self._btn2GPIO.setEvent(edge=GPIO.FALLING, callback=self._ledGreenGPIO.toggle, bouncetime=200)
-        self._btn3GPIO.setEvent(edge=GPIO.FALLING, callback=self._ledBlueGPIO.toggle, bouncetime=200)
-        self._btn4GPIO.setEvent(edge=GPIO.FALLING, callback=self._ledYellowGPIO.toggle, bouncetime=200)
+        GPIO.add_event_detect(self._btns[0], edge=GPIO.RISING, callback=lambda _: self._toggleLed(0), bouncetime=200)
+        GPIO.add_event_detect(self._btns[1], edge=GPIO.RISING, callback=lambda _: self._toggleLed(1), bouncetime=200)
+        GPIO.add_event_detect(self._btns[2], edge=GPIO.RISING, callback=lambda _: self._toggleLed(2), bouncetime=200)
+        GPIO.add_event_detect(self._btns[3], edge=GPIO.RISING, callback=lambda _: self._toggleLed(3), bouncetime=200)
+
+    def _toggleLed(self, index):
+        self._ledsStatus[index] = not self._ledsStatus[index]
+        self._update()
+
+    def _update(self):
+        for i in range(len(self._leds)):
+            GPIO.output(self._leds[i], self._ledsStatus[i])
+
+    def _initGui(self):
+        self.root = Tk()
+        self.root.title('Leds')
+
+        frame = LabelFrame(self.root, text = "Led buttons", padx=10, pady=10)
+
+        for i in range(4):
+            frame.columnconfigure(i, minsize=150, weight=1)
+
+        btn_red = Button(frame, text="Red", command=lambda: self._toggleLed(0))
+        btn_red.grid(column=0, row=0, padx=10, pady=10, sticky='ew')
+
+        btn_green = Button(frame, text="Green", command=lambda: self._toggleLed(1))
+        btn_green.grid(column=1, row=0, padx=10, pady=10, sticky='ew')
+
+        btn_blue = Button(frame, text="Blue", command=lambda: self._toggleLed(2))
+        btn_blue.grid(column=2, row=0, padx=10, pady=10, sticky='ew')
+
+        btn_yellow = Button(frame, text="Yellow", command=lambda: self._toggleLed(3))
+        btn_yellow.grid(column=3, row=0, padx=10, pady=10, sticky='ew')
+
+        frame.grid(column=0, row=0, padx=10, pady=10, sticky='ew')
+
+    def _initBT(self):
+        server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+  
+        port = 1
+        server_sock.bind(("",port))
+        server_sock.listen(1)
+        act
+        client_sock,address = server_sock.accept()
+        print("Accepted connection from " + str(address))
+        
+        # data = client_sock.recv(1024)
+        # print("received [%s]" % data)
+        
+        # client_sock.close()
+        # server_sock.close()
+
 
     def _loop(self):
         while True:
-            pass
+            data = client_sock.recv(1024)
+            print(f"received {data}")
+
+            time.sleep(0.5)
+
+            self.root.update()
     
 if __name__ == "__main__":
-    Main()
+    main = Main()
