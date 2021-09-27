07dd8d7510c1f14e6783446588907981332f13d7
Wesley Vercoutere <wesley.vercoutere@gmail.com>
2021-09-27 10:47:23 +0200

    Update RPi3-1

--- RPi-3/RPi3-1/voorbeeld/basic_server_rp.py
+++ RPi-3/RPi3-1/voorbeeld/basic_server_rp.py
@@ -14,29 +14,33 @@ conn = None
 
 # trukje om IP-adres van je RP te dedecteren
 def get_ip_address():
- ip_address = '';
- s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
- s.connect(("8.8.8.8",80))
- print("s.getsockname()=",s.getsockname())
- ip_address = s.getsockname()[0]
- print("ip_address=",ip_address)
- s.close()
- return ip_address
+    ip_address = ''
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8",80))
+    print("s.getsockname()=",s.getsockname())
+    ip_address = s.getsockname()[0]
+    print("ip_address=",ip_address)
+    s.close()
+    return ip_address
 
 try:
     while True:
-
         print("Server is waiting for a connection at",get_ip_address(), "and port", PORT)
         conn, addr = s.accept()
         print('Server got a connection from' , addr)
+        
         while True:
             request = conn.recv(2048)
+            
             if len(request)> 0:
                 print("len request =",len(request))
                 print("message from client=",request.decode("UTF-8"))
+            
             else:
-	    print(“client disconnected”)
+                print("client disconnected")
                 break
+
+                
 
 except Exception as e:
     print("Except>" , e)
