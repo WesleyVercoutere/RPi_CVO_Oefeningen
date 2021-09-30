603812de59b124889924a5a5dd73732b1388471e
WesleyVercoutere <wesley.vercoutere@gmail.com>
2021-09-30 11:48:22 +0200

    Update RPi3-3

--- RPi-3/RPi3-3/Website_RPi_V0.1.2/src/service/ResourceHelper.py
+++ RPi-3/RPi3-3/Website_RPi_V0.1.2/src/service/ResourceHelper.py
@@ -1,6 +1,23 @@
+import os
+
+from domain.RequestObject import RequestObject
+
+
 class ResourceHelper:
 
     def __init__(self) -> None:
         pass
 
-    
+    def get_resource(self, request_obj: RequestObject):
+        file = self._get_path(request_obj.filename)
+
+        if request_obj.filetype == "html":
+            file = open(file, "r")
+
+        elif request_obj.filetype == "jpg" or request_obj.filetype == "favicon" or request_obj.filetype == "png" :
+            file = open(file, "rb")
+        
+        return file
+
+    def _get_path(self, filename):
+        return os.path.join(os.getcwd(), "src", "resources", filename)
--- RPi-3/RPi3-3/Website_RPi_V0.1.2/src/service/ResponseHandler.py
+++ RPi-3/RPi3-3/Website_RPi_V0.1.2/src/service/ResponseHandler.py
@@ -11,43 +11,41 @@ class ResponseHandler:
         self._resource_helper = ResourceHelper()
 
     def get_response(self, request_obj: RequestObject) -> ResponseObject:
-        path = os.getcwd()
-        file = os.path.join(path, "src", "resources", request_obj.filename)
-
         response = ResponseObject()
 
-        if request_obj.filetype == "html":
-            file = open(file, "r")
+        try:
+            file = self._resource_helper.get_resource(request_obj)
+            content = file.read()
+            content_length = len(content)
 
-        elif request_obj.filetype == "jpg" or request_obj.filetype == "favicon" or request_obj.filetype == "png" :
-            file = open(file, "rb")
-        
-        else:
-            raise Exception("Couldn't open file in ResponseHandler.get_response()")
+            response.header_1 = b"HTTP/1.1 200 OK\r\n"
 
-        content = file.read()
-        content_length = len(content)
+            if request_obj.filetype == "html":
+                response.header_2 = b"Content-Type: text/html\r\n"
+            
+            elif request_obj.filetype == "jpg":
+                response.header_2 = b"Content-Type: imgage/jpg\r\n"
+            
+            elif request_obj.filetype == "png":
+                response.header_2 = b"Content-Type: image/png\r\n"
+            
+            elif request_obj.filetype == "favicon":
+                response.header_2 = b"Content-Type: image/ico\r\n"
+            
+            else:
+                raise Exception("No filetype in ResponseHandler.get_response()")
 
-        response.header_1 = b"HTTP/1.1 200 OK\r\n"
+            response.content_length = (f"Content-Length:{str(content_length)}\r\n\r\n").encode("UTF-8")
 
-        if request_obj.filetype=="html":
-            response.header_2 = b"Content-Type: text/html\r\n"
-        elif request_obj.filetype=="jpg":
-            response.header_2 = b"Content-Type: imgage/jpg\r\n"
-        elif request_obj.filetype=="png":
-            response.header_2 = b"Content-Type: image/png\r\n"
-        elif request_obj.filetype=="favicon":
-            response.header_2 = b"Content-Type: image/ico\r\n"
-        else:
-            raise Exception("No filetype in ResponseHandler.get_response()")
+            if request_obj.filetype == "html":
+                response.content = content.encode("UTF-8")
+            else:
+                response.content = content
 
+        except:
+            print("file not found")
+            response.header_1 = b"HTTP/1.1 404 Not Found\r\n"
 
-        response.content_length = (f"Content-Length:{str(content_length)}\r\n\r\n").encode("UTF-8")
-
-        if request_obj.filetype == "html":
-            response.content = content.encode("UTF-8")
-        else:
-            response.content = content
-
-        return response
+        finally:
+            return response
          
