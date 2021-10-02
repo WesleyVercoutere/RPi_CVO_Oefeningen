dd22858be1a3f3434a746dd1473c50866b8a27bf
Wesley Vercoutere <wesley.vercoutere@gmail.com>
2021-10-02 11:14:58 +0200

    Update RPi3-3

--- RPi-3/RPi3-4/ESP32_Control_led_via_web_V0.1.0/boot.py
+++ RPi-3/RPi3-4/ESP32_Control_led_via_web_V0.1.0/boot.py
@@ -7,7 +7,9 @@ def do_connect():
     if not sta_if.isconnected():
         print('connecting to network...')
         sta_if.active(True) # ESP acts as a station and connects to an acces point
+        sta_if.ifconfig(('192.168.0.20', '255.255.255.0', '192.168.0.1', '8.8.8.8'))
         sta_if.connect('telenet-69249', '5E40nQkE42tk') # use your 
+
         while not sta_if.isconnected():
             pass
     print('network config:', sta_if.ifconfig())  #network config: ('192.168.0.110', '255.255.255.0', '192.168.0.1', '42.2.24.0')
