145e0718bf166b3ec5f386d5734416d073bcdad1
WesleyVercoutere <wesley.vercoutere@gmail.com>
2021-10-04 12:13:30 +0200

    Update RPi3-4

--- RPi-3/RPi3-4/ESP32_Control_led_via_web_V0.1.0/boot.py
+++ RPi-3/RPi3-4/ESP32_Control_led_via_web_V0.1.0/boot.py
@@ -6,12 +6,13 @@ def do_connect():
     sta_if = network.WLAN(network.STA_IF)
     if not sta_if.isconnected():
         print('connecting to network...')
-        sta_if.active(True) # ESP acts as a station and connects to an acces point
+        sta_if.active(True)
         sta_if.ifconfig(('192.168.0.20', '255.255.255.0', '192.168.0.1', '8.8.8.8'))
-        sta_if.connect('SSID', 'KEY') # use your 
+        sta_if.connect('SSID', 'KEY')
 
         while not sta_if.isconnected():
             pass
+        
     print('network config:', sta_if.ifconfig())  #network config: ('192.168.0.110', '255.255.255.0', '192.168.0.1', '42.2.24.0')
     
 do_connect()
