11cdced93456ae01910b1203745555e90f5b880c
WesleyVercoutere <wesley.vercoutere@gmail.com>
2021-10-19 12:48:38 +0200

    Update RPi3-6

---
+++ RPi-3/RPi3-6/ESP32_Control_led_via_web_V0.3.2/resources/js/index.js
@@ -0,0 +1,22 @@
+window.onload = (event) => {
+
+    console.log("Window loaded");
+
+    var counter = 0;
+
+    setInterval(function() {
+
+        counter = updateCounter(counter);
+        console.log(counter);
+
+    }, 1000);
+
+}
+
+function updateCounter(counter) {
+    return counter += 1;
+}
+
+function btnClicked() {
+    alert('clicked');
+}
